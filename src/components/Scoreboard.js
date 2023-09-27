0c649d0b0f2f36f265e94fd6742e360b653d6c3e
Nicholas Poet <npoet@allathlete.com>
2023-09-26 18:11:34 -0700

    update component to link TV channels to Fubo

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,19 +2,47 @@
 
 import React from 'react';
 
+const tvChannelIds = {
+    "ABC": 19570,
+    "ESPN": 10179,
+    "ESPN2": 12444,
+    "ESPNU": 71094,
+    "SECN": 1021370001,
+    "ACCN": 1263880001,
+    "FOX": 20450,
+    "FS1": 94653,
+    "FS2": 69553,
+    "BTN": 71605,
+    "PAC12": 88172,
+    "CBS": 19567,
+    "CBSSN": 69495,
+    "NFL NET": 45409,
+    "NBC": 19568
+};
+
 const Scoreboard = ({ data }) => {
-    const { home, home_mascot, home_logo, away, away_mascot, away_logo, time, tv, odds } = data;
+    const {
+        home,
+        home_mascot,
+        home_logo,
+        away,
+        away_mascot,
+        away_logo,
+        time,
+        tv,
+        odds
+    } = data;
+
+    const generateTVLink = (channel) => {
+        if (tvChannelIds[channel]) {
+            const url = `https://www.fubo.tv/watch?channelId=${tvChannelIds[channel]}`;
+            return <a href={url} target="_blank" rel="noopener noreferrer">{channel}</a>;
+        }
+        return channel;
+    };
 
     return (
         <div className="scoreboard">
-            <div className="team">
-                <img src={away_logo} alt={`${away} Logo`} />
-                <div className="info">
-                    <h2>{away}</h2>
-                    <p>{away_mascot}</p>
-                </div>
-            </div>
-            <div className="versus">VS</div>
             <div className="team">
                 <img src={home_logo} alt={`${home} Logo`} />
                 <div className="info">
@@ -22,10 +50,17 @@ const Scoreboard = ({ data }) => {
                     <p>{home_mascot}</p>
                 </div>
             </div>
-            
+            <div className="versus">VS</div>
+            <div className="team">
+                <img src={away_logo} alt={`${away} Logo`} />
+                <div className="info">
+                    <h2>{away}</h2>
+                    <p>{away_mascot}</p>
+                </div>
+            </div>
             <div className="game-details">
                 <p>{time}</p>
-                <p>TV: {tv}  |  Odds: {odds}</p>
+                <p>TV: {generateTVLink(tv)}   |   Odds: {odds}</p>
             </div>
         </div>
     );
