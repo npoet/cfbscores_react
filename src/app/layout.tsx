d1581c7560852018e5ec6d96714a3f5181c79511
Nicholas Poet <npoet@allathlete.com>
2023-09-28 19:04:08 -0700

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'CFBScores',
+  title: 'FBscores',
   description: 'Live Scores and Schedules',
 }
 
