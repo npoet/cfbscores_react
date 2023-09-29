d0d7e769644c1be6c7276ce42851d4ab6766aaa3
Nicholas Poet <npoet@allathlete.com>
2023-09-28 19:03:30 -0700

    move tv link gen to lib file, update

--- src/components/Live.js
+++ src/components/Live.js
@@ -1,74 +1,53 @@
 // Live.js
 
 import React from 'react';
+import GenerateTVLink from '../lib/TvLinks';
 import './Scoreboard.css';
-
-const tvChannelIds = {
-    "ABC": 19570,
-    "ESPN": 10179,
-    "ESPN2": 12444,
-    "ESPNU": 71094,
-    "SECN": 1021370001,
-    "ACCN": 1263880001,
-    "FOX": 20450,
-    "FS1": 94653,
-    "FS2": 69553,
-    "BTN": 71605,
-    "PAC12": 88172,
-    "CBS": 19567,
-    "CBSSN": 69495,
-    "NFL NET": 45409,
-    "NBC": 19568
-};
 
 const Live = ({ data }) => {
     const {
         home,
+        home_site,
         home_logo,
         home_score,
         home_record,
         away,
+        away_site,
         away_logo,
         away_score,
         away_record,
         time,
-        quarter,
+        possession,
+        ball_on,
         tv
     } = data;
-
-    const generateTVLink = (channel) => {
-        if (tvChannelIds[channel]) {
-            const url = `https://www.fubo.tv/watch?channelId=${tvChannelIds[channel]}`;
-            return <a href={url} target="_blank" rel="noopener noreferrer">{channel}</a>;
-        }
-        return channel;
-    };
 
     return (
         <div className="live-game">
             <div className="team">
                 <img src={away_logo} alt={`${away} Logo`} />
                 <div className="info">
-                    <h2>{away}</h2>
+                    <h2><a href={away_site} target="_blank" rel="noopener noreferrer">{away}</a></h2>
                     <p>{away_record}</p>
                 </div>
+                {possession == "away" && <div className="poss">{'\u2B24'}</div>}
                 <div className="score">{away_score}</div>
             </div>
-            <p>{away_record}</p>
             <div className="versus">VS</div>
             <div className="team">
                 <img src={home_logo} alt={`${home} Logo`} />
                 <div className="info">
-                    <h2>{home}</h2>
+                    <h2><a href={home_site} target="_blank" rel="noopener noreferrer">{home}</a></h2>
                     <p>{home_record}</p>
                 </div>
+                {possession == "home" && <div className="poss">{'\u2B24'}</div>}
                 <div className="score">{home_score}</div>
             </div>
-            <p>{home_record}</p>
+            {ball_on != "" && <div className="game-clock">{time}  |  {ball_on}</div>}
+            {ball_on == "" && <div className="game-clock">{time}</div>}
+
             <div className="game-details">
-                <p>Time: {time}</p>
-                <p>Quarter: {quarter}</p>
-                <p>TV: {generateTVLink(tv)}</p>
+                <p>TV: {GenerateTVLink(tv)}</p>
             </div>
         </div>
     );
---
+++ src/lib/TvLinks.js
@@ -0,0 +1,31 @@
+// TvLinks.js
+
+import React from 'react';
+
+const tvChannelIds = {
+    "ABC": 19570,
+    "ESPN": 10179,
+    "ESPN2": 12444,
+    "ESPNU": 71094,
+    "SECN": 1021370001,
+    "ACCN": 1263880001,
+    "FOX": 20450,
+    "FS1": 94653,
+    "FS2": 69553,
+    "BTN": 71605,
+    "PAC12": 88172,
+    "CBS": 19567,
+    "CBSSN": 69495,
+    "NFL NET": 45409,
+    "NBC": 19568
+};
+
+const GenerateTVLink = (channel) => {
+    if (tvChannelIds[channel]) {
+        const url = `https://www.fubo.tv/watch?channelId=${tvChannelIds[channel]}`;
+        return <a href={url} target="_blank" rel="noopener noreferrer">{channel}</a>;
+    }
+    return channel;
+};
+
+export default GenerateTVLink;
