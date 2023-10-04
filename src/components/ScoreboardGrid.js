e31889dad872b8c6aed6bf3d41d714ddc2dc2900
Nicholas Poet <npoet@allathlete.com>
2023-10-04 10:29:35 -0700

    add filters, top bar

--- src/components/ScoreboardGrid.js
+++ src/components/ScoreboardGrid.js
@@ -3,9 +3,12 @@
 import React, { useState, useEffect } from 'react';
 import Scoreboard from './Scoreboard';
 import './ScoreboardGrid.css';
+import TopBar from './TopBar'
 
 const ScoreboardGrid = () => {
     const [scoreboardDataList, setScoreboardDataList] = useState([]);
+    const [expandedScoreboards, setExpandedScoreboards] = useState([]);
+    const [filter, setFilter] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -13,6 +16,7 @@ const ScoreboardGrid = () => {
                 const response = await fetch('http://127.0.0.1:8000/scores');
                 const data = await response.json();
                 setScoreboardDataList(data);
+                setExpandedScoreboards(Array(data.length).fill(false)); // Initialize all scoreboards as not expanded
             } catch (error) {
                 console.error('Error fetching scoreboard data:', error);
             }
@@ -27,18 +31,54 @@ const ScoreboardGrid = () => {
         return () => clearInterval(refreshInterval);
     }, []);
 
+    const handleScoreboardClick = (index) => {
+        const newExpandedScoreboards = [...expandedScoreboards];
+        newExpandedScoreboards[index] = !newExpandedScoreboards[index];
+        setExpandedScoreboards(newExpandedScoreboards);
+    };
+
+    const handleExpandAll = () => {
+        setExpandedScoreboards(Array(scoreboardDataList.length));
+    };
+
+    const handleFilterChange = (category) => {
+        setFilter(category);
+    };
+
+    const handleFilterReset = () => {
+        setFilter(null);
+    };
+
+    const filteredScoreboards = filter
+        ? scoreboardDataList.filter((scoreboard) => scoreboard.type === filter)
+        : scoreboardDataList;
+
     return (
-        <div className="scoreboard-grid">
-            {scoreboardDataList.map((scoreboardData, index) => {
-                const isLive = scoreboardData.home_score !== undefined;
-                return (
-                    <Scoreboard
-                        key={index}
-                        data={scoreboardData}
-                        isLive={isLive}
-                    />
-                );
-            })}
+        <div>
+            <TopBar
+                onExpandAll={handleExpandAll}
+                onFilterChange={handleFilterChange}
+                onFilterReset={handleFilterReset}
+            />
+            <div className="scoreboard-grid">
+                {filteredScoreboards.map((scoreboardData, index) => {
+                    var isLive = scoreboardData.home_score !== undefined;
+                    const isFinal = scoreboardData.time == "Final";
+                    if (isFinal) {
+                        isLive = false
+                    }
+                    return (
+                        <div key={index} onClick={() => handleScoreboardClick(index)}>
+                            <Scoreboard
+                                key={index}
+                                data={scoreboardData}
+                                isLive={isLive}
+                                isFinal={isFinal}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
