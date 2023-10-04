160dd57333d4adcf4acb747f99efc33b7cb5ed57
Nicholas Poet <npoet@allathlete.com>
2023-10-04 10:29:56 -0700

    add conditionals per type

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,16 +1,37 @@
 // Scoreboard.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import Live from './Live'
+import Final from './Final'
+import Stats from './Stats'
 import Scheduled from './Scheduled'
 
-const Scoreboard = ({ data, isLive }) => {
+const Scoreboard = ({ data, isLive, isFinal }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleExpansion = () => {
+        setIsExpanded(!isExpanded);
+    };
+
     return (
-        <div className="scoreboard">
+        <div className={`scoreboard ${isExpanded ? 'expanded' : ''}`} onClick={toggleExpansion}>
+            {!isLive && !isFinal ? (
+                <Scheduled data={data} />
+            ) : (
+                <></>
+            )}
             {isLive ? (
                 <Live data={data} />
             ) : (
-                <Scheduled data={data} />
+                <></>
+            )}
+            {isFinal ? (
+                <Final data={data} />
+            ) : (
+                <></>
+            )}
+            {isExpanded && (
+                <Stats data={data} isLive={isLive} />
             )}
         </div>
     );
