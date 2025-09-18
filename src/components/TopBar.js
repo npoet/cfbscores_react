6a00757249f7f59fa9fcbc1fa8f5b0d0bccadaeb
Nick Poet <nick@stoneweavr.com>
2025-09-18 01:39:01 -0700

    adjust top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,48 +1,57 @@
 // TopBar.js
 
-import React from 'react';
-import { FaFootballBall, FaFutbol, FaGlobeAmericas, FaGraduationCap, FaCalendarDay } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import {
+  FaFootballBall,
+  FaFutbol,
+  FaGlobeAmericas,
+  FaGraduationCap,
+  FaCalendarDay,
+} from 'react-icons/fa';
 
 const TopBar = ({ onFilterChange, onFilterReset }) => {
-    const handleAllFootball = () => {
-        onFilterChange(['NFL', 'FBS', 'FCS']);
-    };
 
-    const handleCollegeFootball = () => {
-        onFilterChange(['FBS', 'FCS']);
-    };
+  const handleAllFootball = () => {
+    onFilterChange(['NFL', 'FBS', 'FCS']);
+  };
 
-    const handleAllSoccer = () => {
-        onFilterChange('EPL');
-    };
+  const handleCollegeFootball = () => {
+    onFilterChange(['FBS', 'FCS']);
+  };
 
-    const handleToday = () => {
-        onFilterChange('TODAY');
-    };
+  const handleAllSoccer = () => {
+    onFilterChange('EPL');
+  };
 
-    return (
-        <div className="top-bar">
-            <button onClick={onFilterReset}>
-                <FaGlobeAmericas title="Show All" />
-            </button>
-            <button onClick={handleToday}>
-                <FaCalendarDay title="Today" />
-            </button>
-            <button onClick={handleAllFootball}>
-                <FaFootballBall title="All Football" />
-            </button>
-            <button onClick={handleCollegeFootball}>
-                <FaGraduationCap title="College Football" />
-            </button>
-            <button onClick={handleAllSoccer}>
-                <FaFutbol title="All Soccer" />
-            </button>
-            <button onClick={() => onFilterChange('NFL')}>NFL</button>
-            <button onClick={() => onFilterChange('FBS')}>FBS</button>
-            <button onClick={() => onFilterChange('FCS')}>FCS</button>
-            <button onClick={() => onFilterChange('EPL')}>EPL</button>
-        </div>
-    );
+  const handleToday = () => {
+    onFilterChange('TODAY');
+  };
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  return (
+    <div className="top-bar">
+      <div className="filters">
+        <button onClick={onFilterReset} title="Show All">
+          <FaGlobeAmericas />
+        </button>
+        <button onClick={handleToday} title="Today">
+          <FaCalendarDay />
+        </button>
+        <button onClick={handleAllFootball} title="All Football">
+          <FaFootballBall />
+        </button>
+        <button onClick={handleCollegeFootball} title="College Football">
+          <FaGraduationCap />
+        </button>
+        <button onClick={handleAllSoccer} title="All Soccer">
+          <FaFutbol />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
